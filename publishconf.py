324b329e9d9fa3294fa713ccc3001102997cf6ec
Jason K. Moore <moorepants@gmail.com>
2019-10-16 17:00:39 -0700

    Build for Travis and turn on analytics for my web server.

--- publishconf.py
+++ publishconf.py
@@ -11,15 +11,16 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://moorepants.info'
+if os.environ['TRAVIS'] == 'true':
+    SITEURL = 'https://moorepants.info.github.io'
+else:
+    SITEURL = 'https://moorepants.info'
+    DISQUS_SITENAME = "moorepants"
+    GOOGLE_ANALYTICS = "UA-15966419-6"
+
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
