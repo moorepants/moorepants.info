78e5b599f7bdddc933b41e32934ef91eb06c4eda
Jason K. Moore <moorepants@gmail.com>
2026-01-16 09:07:03 +0100

    Update dreamobject script to boto3.

--- dreamobject.py
+++ dreamobject.py
@@ -1,22 +1,53 @@
+# Create a ~/.aws/credentials file with:
+#[Credentials]
+#aws_access_key_id = <key>
+#aws_secret_access_key = <key>
+
 import os
 
-import boto
+import boto3
+from botocore.exceptions import ClientError
 
-conn = boto.connect_s3(host='objects-us-east-1.dream.io')
-
-bucket = conn.get_bucket('moorepants')
-
+accepted_file_exts = ['.pdf', '.png', '.gif', '.mp4', '.jpg', '.svg']
+BUCKET_NAME = 'moorepants'
+HOST = 's3.us-east-005.dream.io'
 assets_dir = 'assets'
 
-files = os.listdir(assets_dir)
 
-for fname in files:
-    key = boto.s3.key.Key(bucket, fname)
-    if key.exists():
-        print('Skipping: {} (already present in the bucket)'.format(fname))
-    else:
-        print('Uploading: {}'.format(fname))
-        key.set_contents_from_filename(os.path.join(assets_dir, fname))
+def upload(overwrite=False):
 
-for o in bucket.list():
-    o.set_acl('public-read')
+    session = boto3.session.Session()
+    s3 = session.resource('s3', endpoint_url=f'https://{HOST}')
+    bucket = s3.Bucket(BUCKET_NAME)
+
+    files = os.listdir(assets_dir)
+
+    for fname in files:
+        if os.path.splitext(fname)[-1] in accepted_file_exts:
+            s3_object = bucket.Object(fname)
+            try:
+                s3_object.last_modified
+            except ClientError:
+                print('Uploading: {}'.format(fname))
+                s3_object.upload_file(os.path.join(assets_dir, fname))
+                s3_object.Acl().put(ACL='public-read')
+            else:
+                if overwrite is False:
+                    msg = 'Skipping: {} (already present in the bucket)'
+                    print(msg.format(fname))
+                else:
+                    print('Overwriting, uploading: {}'.format(fname))
+                    s3_object.upload_file(os.path.join(assets_dir, fname))
+                    s3_object.Acl().put(ACL='public-read')
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Upload Files to Dreamhost.')
+    parser.add_argument('--overwrite', action='store_true',
+                        help="Forces files to be overwritten.")
+    args = parser.parse_args()
+
+    upload(overwrite=args.overwrite)
